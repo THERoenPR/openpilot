14b516e7604bdca7a9989eefb05e5c0796528f47
THERoenPR <236117023+THERoenPR@users.noreply.github.com>
2025-11-05 00:22:22 -0500

    Apply FP modified limit_accel_in_turns()
    Added conditional for minimum lateral acceleration
    
    Co-Authored-By: James <91348155+frogai@users.noreply.github.com>

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -15,6 +15,8 @@ from openpilot.selfdrive.controls.lib.drive_helpers import CONTROL_N, get_accel_
 from openpilot.selfdrive.car.cruise import V_CRUISE_MAX, V_CRUISE_UNSET
 from openpilot.common.swaglog import cloudlog
 
+from roenpilot.common.roenpilot_variables import MINIMUM_LATERAL_ACCELERATION
+
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
@@ -43,7 +45,7 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
   # The lookup table for turns should also be updated if we do this
   a_total_max = np.interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
   a_y = v_ego ** 2 * angle_steers * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
-  a_x_allowed = math.sqrt(max(a_total_max ** 2 - a_y ** 2, 0.))
+  a_x_allowed = math.sqrt(max(a_total_max ** 2 - a_y ** 2, 0.)) if abs(a_y) > MINIMUM_LATERAL_ACCELERATION else a_target[1]
 
   return [a_target[0], min(a_target[1], a_x_allowed)]
 
